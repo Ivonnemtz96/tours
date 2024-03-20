6265b84a376b8e86dc6cfad244468496b9ccb969
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-03-20 09:55:23 -0700

    se quita cursor

--- includes/preloader.php
+++ includes/preloader.php
@@ -1,5 +1,5 @@
-<div class="custom-cursor__cursor"></div>
-<div class="custom-cursor__cursor-two"></div>
+<!-- <div class="custom-cursor__cursor"></div>
+<div class="custom-cursor__cursor-two"></div> -->
 
 <div class="preloader">
     <div class="preloader__image" style="background-image: url(assets/images/loader.png);"></div>
